18e07ad03e90396b1d257a602f2ad7cb004475d0
muca <mucisan22@gmail.com>
2018-03-22 12:16:32 +0100

    speelveld en vak toegevoegd

---
+++ Code3G/src/code3g/Vak.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package code3g;
+
+/**
+ *
+ * @author mucis
+ */
+public class Vak {
+    private String kleur;
+    
+    public Vak(String gekozenKleur) // verwacht een hexCode kleur
+    {
+        this.kleur = kleur;
+    }
+    
+    public void setKleur(String gekozenKleur)
+    {
+        this.kleur = kleur;
+    }
+    
+    public String getKleur()
+    {
+        return this.kleur;
+    }
+}
