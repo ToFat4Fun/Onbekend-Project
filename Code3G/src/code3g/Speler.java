555954ab7f0103eb974c192b8d0d94414d08de7a
Tarikkc <37372935+Tarikkc@users.noreply.github.com>
2018-03-22 12:43:32 +0100

    speler toegevoegd

---
+++ Code3G/src/code3g/Speler.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package code3g;
+
+/**
+ *
+ * @author Ta
+ */
+public class Speler {
+    
+}
