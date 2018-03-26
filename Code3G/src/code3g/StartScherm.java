851cb42fc95756707427084951ff257b35a62c0e
muca <mucisan22@gmail.com>
2018-03-26 23:04:10 +0200

    knoppen toegevoegd aan speelveld

--- Code3G/src/code3g/StartScherm.java
+++ Code3G/src/code3g/StartScherm.java
@@ -19,55 +19,77 @@ import javax.swing.JPanel;
  *
  * @author jurian
  */
-public class StartScherm extends JFrame{
+public class StartScherm extends JFrame {
 
     ArrayList<Eigenschap> eigenschappen = new ArrayList<Eigenschap>();
     private MoeilijkheidsGraad moeilijkheidsGraad; // moeilijkheidsgraad declareren
-    private JButton[] button = new JButton[]
-                {
-                    new JButton(MoeilijkheidsGraad.makkelijk.name()), //bron :https://stackoverflow.com/questions/3978654/best-way-to-create-enum-of-strings
-                    new JButton(MoeilijkheidsGraad.normaal.name()),
-                    new JButton(MoeilijkheidsGraad.moeilijk.name())
-                };
-   
+    private JButton[] button = new JButton[]{
+        new JButton(MoeilijkheidsGraad.makkelijk.name()), //bron :https://stackoverflow.com/questions/3978654/best-way-to-create-enum-of-strings
+        new JButton(MoeilijkheidsGraad.normaal.name()),
+        new JButton(MoeilijkheidsGraad.moeilijk.name())
+    };
+
+    private JButton[] speelveldButton = new JButton[]{
+        new JButton("Start"),
+        new JButton("Restart"),
+        new JButton("Stoppen")
+    };
+
     public enum MoeilijkheidsGraad { // bron: https://stackoverflow.com/questions/3978654/best-way-to-create-enum-of-strings
         makkelijk, normaal, moeilijk
     } // moeilijkheidsGraad in enum datatype.
 
     public StartScherm() {
-        
+
         new JFrame();
-        
-        JPanel BottomPanel = new JPanel();// sigh zelfde als hierboven
+
+        JPanel BottomPanel = new JPanel();// Paneel voor 3 knoppen aanmaken
         JLabel text = new JLabel("  kies moeilijkheidsgraad"); //beetje spatie toevoegen
-        BottomPanel.setLayout(new GridLayout(1,3));
-        
+        BottomPanel.setLayout(new GridLayout(1, 3));
+
         BottomPanel.add(button[0]);
         BottomPanel.add(button[1]);
         BottomPanel.add(button[2]);
-        
-        add(text,BorderLayout.CENTER);
-        add(BottomPanel,BorderLayout.SOUTH);
-        
-        button[0].addActionListener(new ActionListener() 
-        {
-         @Override
-         public void actionPerformed(ActionEvent event) 
-         {         
-               System.out.println(button[0].getText());
-               setVisible(false);
-               OpenSpeelveld("test");
-         } 
+
+        add(text, BorderLayout.CENTER);
+        add(BottomPanel, BorderLayout.SOUTH);
+
+        button[0].addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent event) {
+                System.out.println(button[0].getText());
+                setVisible(false);
+                OpenSpeelveld("test");
+            }
         }
         );
-      }
+    }
+
     //moeilijkheidsgraad meegeven
-    public void OpenSpeelveld(String Graad){
-        
+    public void OpenSpeelveld(String Graad) {
         Speelveld speelveld = new Speelveld();
-    
+
         JFrame f = new JFrame();
         f.add(speelveld);
+        JPanel BottomPanel = new JPanel();// Paneel voor 3 knoppen aanmaken
+        BottomPanel.setLayout(new GridLayout(1, 3));
+
+        BottomPanel.add(speelveldButton[0]);
+        BottomPanel.add(speelveldButton[1]);
+        BottomPanel.add(speelveldButton[2]);
+        f.add(BottomPanel, BorderLayout.SOUTH);
+        speelveldButton[0].addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent event) {
+                System.out.println(speelveldButton[0].getText());
+                speelveldButton[0].setFocusable(false);
+                speelveldButton[1].setFocusable(false);
+                speelveldButton[2].setFocusable(false);
+                speelveld.setFocusable(true);// this applicatie focuseren, om een toetsenbord werken te krijgen.
+
+            }
+        }
+        );
 
         f.setTitle("BarricadeSleutel spel door groep 3.5.7 2018");// naam van het scherm
         f.setSize(700, 800);// breedte en hoogte instellen
@@ -109,7 +131,7 @@ public class StartScherm extends JFrame{
             {0, 0, 100, 900, 500, 100, 0, 0, 0, 0},
             {200, 0, 100, 50, 0, 0, 0, 0, 0},
             {0, 0, 100, 150, 0, 0, 0, 0, 0, 0}
-        }; 
+        };
 
         for (int i = 0; i < map.length; i++) {
             for (int j = 0; j < map[i].length; j++) {
@@ -136,5 +158,5 @@ public class StartScherm extends JFrame{
         //speelveld.addEigenschappenForVakken(eigenschappen);
         speelveld.setVisible(true);//open het applicatie/scherm.  
     }
-    
+
 }
