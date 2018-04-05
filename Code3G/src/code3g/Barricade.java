81bc7adcef2fc27f37600c59c4983e694f186c43
Jurian <jurian1998@live.com>
2018-04-05 21:41:00 +0200

    updated headers ;)

--- Code3G/src/code3g/Barricade.java
+++ Code3G/src/code3g/Barricade.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+   This game is developed by Mucahit Coskun, Jurian van Hoorn and Tarik Sherif, students at The Hague University of Applied Sciences
+   If you wish to you use any of this code please refer to the authors or copy this header.
+   This is our version of a Maze Game project for Software Engineering 2017-2018 3rd semester.
  */
 package code3g;
 
