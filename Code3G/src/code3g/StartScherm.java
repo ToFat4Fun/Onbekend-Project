14a990c56067186abdc7d0f66fb36b5f342fd5b9
Tarikkc <37372935+Tarikkc@users.noreply.github.com>
2018-04-06 17:03:06 +0200

    level behaalbaar gemaakt

--- Code3G/src/code3g/StartScherm.java
+++ Code3G/src/code3g/StartScherm.java
@@ -281,7 +281,7 @@ public class StartScherm extends JFrame {
                 {2, 1, 1, 3, 1, 1, 1, 1, 1, 1},
                 {2, 1, 2, 2, 2, 2, 2, 2, 2, 1},
                 {2, 2, 2, 1, 1, 1, 1, 1, 2, 1},
-                {2, 3, 1, 1, 1, 1, 1, 1, 1, 4}
+                {2, 3, 1, 1, 1, 1, 1, 1, 0, 4}
             };
 
             makkelijkMapNummer3 = new int[][]{
